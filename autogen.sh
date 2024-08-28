a5d84ae2badfbcc2378bc75512a237daefe9a019
ace2nutzer <ace2nutzer@gmail.com>
2024-08-28 18:56:46 +0200

    autogen.sh: update

--- autogen.sh
+++ autogen.sh
@@ -13,6 +13,14 @@ fi
 
 MAKEFLAGS=""
 
-autoreconf -v --install || exit 1
+autoreconf --force -v --install || exit 1
 
-"$srcdir"/configure "$@"
+FLAGS_CPU="-march=core2 -mcpu=core2 -mtune=core2"
+FLAGS="-O2 -ftree-vectorize -fomit-frame-pointer -fno-strict-aliasing \
+	-Wno-stringop-overflow -Wno-redundant-decls -Wno-unused-variable \
+	-Wno-unused-function -Wno-unused-but-set-variable -DNDEBUG -pipe"
+
+"$srcdir"/configure \
+		CFLAGS="$FLAGS_CPU $FLAGS -Werror-implicit-function-declaration" \
+		CXXFLAGS="$FLAGS_CPU $FLAGS" \
+		--prefix=/usr "$@"
