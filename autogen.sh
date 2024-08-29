d72b49614cfe4447a7b35b7dff08864aab541ad6
ace2nutzer <ace2nutzer@gmail.com>
2024-08-29 20:37:57 +0200

    autogen.sh: update

--- autogen.sh
+++ autogen.sh
@@ -3,24 +3,20 @@
 srcdir=`dirname "$0"`
 test -z "$srcdir" && srcdir=.
 
-SRCDIR=`(cd "$srcdir" && pwd)`
 ORIGDIR=`pwd`
-
-if test "x$SRCDIR" != "x$ORIGDIR"; then
-	echo "Mesa cannot be built when srcdir != builddir" 1>&2
-	exit 1
-fi
-
-MAKEFLAGS=""
+cd "$srcdir"
 
 autoreconf --force -v --install || exit 1
+cd "$ORIGDIR" || exit $?
 
-FLAGS_CPU="-march=core2 -mcpu=core2 -mtune=core2"
+FLAGS_CPU="-march=native -mcpu=native -mtune=native"
 FLAGS="-O2 -ftree-vectorize -fomit-frame-pointer -fno-strict-aliasing \
 	-Wno-stringop-overflow -Wno-redundant-decls -Wno-unused-variable \
 	-Wno-unused-function -Wno-unused-but-set-variable -DNDEBUG -pipe"
 
-"$srcdir"/configure \
+if test -z "$NOCONFIGURE"; then
+    exec "$srcdir"/configure \
 		CFLAGS="$FLAGS_CPU $FLAGS -Werror-implicit-function-declaration" \
 		CXXFLAGS="$FLAGS_CPU $FLAGS" \
 		--prefix=/usr "$@"
+fi
